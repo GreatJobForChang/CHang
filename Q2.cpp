2abc8ccb75b6357277e134410c4a591751a1df6e
GreatJobForChang <90063989+GreatJobForChang@users.noreply.github.com>
2021-10-30 13:01:22 -0400

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+class Student
+{
+    public:
+        Student()
+        {}
+        Student(int ID)
+        {
+            this->ID=ID;
+        }
+        int getID()
+        {
+            return ID;
+        }
+        void ShowMajor()
+        {
+            cout<<"I am a student in Miami University\n";
+        }
+    protected:
+        int ID;
+};
+
+
+
+class CSStudent: public Student {
+//=====TO DO==================
+//complete the definition for CSStudent
+	void ShowMajor() 
+	{
+	    cout<<"I am a student majoring Computer Science\n";
+	    cout<<"And My ID is"<<ID<<endl;
+	}
+};
+
+
+
+class SEStudent: public Student {
+//=====TO DO==================
+//complete the definition for SEStudent
+	void ShowMajor() 
+	{
+	    cout<<"I am a student majoring Software Engineering\n";
+	    cout<<"And My ID is"<<ID<<endl;
+	}
+};
+
+int main()
+{
+    //Student::ShowMajor();
+    CSStudent CSA(25);
+    CSA.ShowMajor();
+    SEStudent SEB(22);
+    SEB.ShowMajor();
+}
