b6cc4222857817ba0a01346c635f12d65cf228c6
GreatJobForChang <90063989+GreatJobForChang@users.noreply.github.com>
2021-11-12 14:20:24 -0500

    Add files via upload

---
+++ Demoa.cpp
@@ -0,0 +1,29 @@
+#define MYSQLPP_MYSQL_HEADERS_BURIED
+#include <mysql++/mysql++.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    // Connect to database with: database, server, userID, password
+    mysqlpp::Connection myDB("cse278","localhost" , "cse278",
+            "student123");
+
+
+	// More code to come...
+    mysqlpp::Query query = myDB.query();        
+    query << "SELECT pname, price FROM Product;";
+    query.parse();  // check to ensure it is correct
+    mysqlpp::StoreQueryResult result = query.store();
+    // Result is in 2-D vector of mysqlpp::String objects
+    for (size_t row = 0; row < result.size(); row++) {
+        std::string pname = result[row][0].c_str();
+        float price = result[row][1];
+        std::cout << pname << "\t" << price << "\n";
+    } // done printing results
+
+    //cout<<query<<endl;
+
+
+}
