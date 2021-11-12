18a2265771f853f98e3f17f014a2a8980ea8c549
liuc62 <liuc62@ceclnx01-19.csi.miamioh.edu>
2021-11-12 15:07:49 -0500

    Demoa.cpp

--- Demoa.cpp
+++ Demoa.cpp
@@ -8,11 +8,28 @@ using namespace std;
 int main(int argc, char* argv[]) {
     // Connect to database with: database, server, userID, password
     mysqlpp::Connection myDB("cse278","localhost" , "cse278",
-            "student123");
+            "S3rul3z");
 
 
 	// More code to come...
-    mysqlpp::Query query = myDB.query();        
+    mysqlpp::Query query = myDB.query();
+    query << "CREATE TABLE UserInfo(\n
+	     User_ID VARCHAR(255) NOT NULL,\n
+	     CarPlate VARCHAR(255) NOT NULL,\n
+	     UserCountry VARCHAR(255) NOT NULL,\n
+	     PRIMARY KEY(User_ID)\n
+	     );";
+    query.parse();
+    query.store(); 
+    query << "CREATE TABLE CarInfo(\n
+ 	    CarPlate VARCHAR(255) NOT NULL,\n
+	    ManufactCountry VARCHAR(255) NOT NULL,\n
+	    CarPrice FLOAT NOT NULL,\n
+	    PRIMARY KEY(CarPlate)\n
+	    );";
+    query.parse();
+    query.store();
+       
     query << "SELECT pname, price FROM Product;";
     query.parse();  // check to ensure it is correct
     mysqlpp::StoreQueryResult result = query.store();
